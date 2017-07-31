8282ab3097800702d47f01891804e5749b248875
neonnishant <neonnishant123@gmail.com>
2017-07-31 15:17:41 +0530

    added project structure

---
+++ WebssCore/src/Server.java
@@ -0,0 +1,17 @@
+
+public class Server {
+
+	private int SERVER_PORT=9877;
+	public Server(){
+		
+	}
+	
+	public Server(int portno){
+		this.SERVER_PORT=portno;
+	}
+	
+	public void startServer(){
+
+		
+	}
+}
